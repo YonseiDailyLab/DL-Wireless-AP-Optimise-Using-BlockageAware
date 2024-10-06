e1a4f7348733b6e239d605a209a2c0d6791bccbc
Marvic1130 <marvic1130@gmail.com>
2024-10-06 23:36:14 +0900

    Update create_datasets.py
    test

--- create_datasets.py
+++ create_datasets.py
@@ -2,81 +2,38 @@ import os
 import logging
 import pandas as pd
 import numpy as np
-from tqdm import trange
 import torch
+from torch.utils.data import DataLoader
+from tqdm import trange
 
-from datasets import CubeObstacle, CylinderObstacle, ChannelDataset
+from datasets import CubeObstacle, CylinderObstacle, BlockageDataset
 from utils.config import Hyperparameters as hparams
-from utils.tools import calc_sig_strength, calc_sig_strength_gpu
+from utils.tools import calc_min_dist
 
 logging.basicConfig(level=logging.INFO)
 
 def createDirectory(directory):
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    except OSError:
-        print("Error: Failed to create the directory.")
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 if __name__ == "__main__":
 
+    # Create obstacles and convert to torch tensors
     obstacle_ls = [
         CubeObstacle(-30, 15, 35, 60, 20),
         CubeObstacle(-30, -25, 45, 10, 35),
         CylinderObstacle(0, -30, 70, 10)
     ]
 
-    for obstacle in obstacle_ls: obstacle.to_torch(hparams.device)
-    gnd_nodes = []
+    # Create dataset
+    dataset = BlockageDataset(40000, obstacle_ls)
+    logging.info(f"len(dataset): {len(dataset)}")
+    dataloader = DataLoader(dataset)
+    for i, data in enumerate(dataloader):
+        station_pos, gnd_nodes, obst_points = data
+        station_pos = station_pos.squeeze()
+        gnd_nodes = gnd_nodes.squeeze()
+        obst_points = obst_points.squeeze()
 
-    for _ in trange(200 * 200 * 4, desc="Generating gnd_nodes..."):
-        temp = []
-        while True:
-            if len(temp) == hparams.num_node:
-                break
-            x = np.random.rand() * hparams.area_size - hparams.area_size // 2
-            y = np.random.rand() * hparams.area_size - hparams.area_size // 2
-            z = 0
-
-            if (x, y) not in temp:
-                is_inside = False
-                for obstacle in obstacle_ls:
-                    if is_inside := obstacle.is_inside(x, y, z):
-                        break
-                if not is_inside: temp.append((x, y, z))
-
-        gnd_nodes.append(temp)
-
-    result_ls = []
-    X, Y = np.meshgrid(
-        np.arange(-hparams.area_size//2, hparams.area_size//2),
-        np.arange(-hparams.area_size//2, hparams.area_size//2),
-        indexing='xy'
-    )
-    Z = np.full_like(X, 70)
-
-    station_positions = torch.tensor(np.stack((X, Y, Z), axis=-1).reshape(-1, 3), dtype=torch.float32).to(hparams.device)
-    gnd_nodes = torch.tensor(np.array(gnd_nodes), dtype=torch.float32).to(hparams.device)
-
-    for i in trange(200 * 200 * 4, desc="Generating dataset"):
-
-        sig = np.array([calc_sig_strength_gpu(station_pos, gnd_nodes, obstacle_ls) for station_pos in station_positions])
-        sig = sig.reshape(hparams.area_size, hparams.area_size)
-
-        max_idx = np.unravel_index(np.argmax(sig), sig.shape)
-        # logging.info(f"Max Signal: {sig[max_idx]}, Index: {max_idx}")
-
-        result_ls.append({
-            "gnd1": gnd_nodes[0],
-            "gnd2": gnd_nodes[1],
-            "gnd3": gnd_nodes[2],
-            "gnd4": gnd_nodes[3],
-            "result": [int(max_idx[0]), int(max_idx[1]), 70],
-            "sig": sig[max_idx]
-        })
-
-
-    createDirectory("data")
-    df = pd.DataFrame(data=result_ls, columns=["gnd1", "gnd2", "gnd3", "gnd4", "result", "sig"])
-    df.to_csv("data/data.csv", index=False)
-    logging.info("Dataset created successfully!")
+        min_dist = calc_min_dist(station_pos, gnd_nodes, obst_points)
+        logging.info(f"min_dist: {min_dist}")
